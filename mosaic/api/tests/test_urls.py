e4ac765f0fad314c413f0af4b6a88acbb067dbaa
Yury Zharikov <zharikovyuri@gmail.com>
2023-03-11 20:13:46 +0300

    add smoke test

--- mosaic/api/tests/test_urls.py
+++ mosaic/api/tests/test_urls.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from school.models import School
+from rest_framework.test import APIRequestFactory, APIClient
 
 
 class SchoolTest(TestCase):
@@ -22,11 +23,28 @@ class SchoolTest(TestCase):
             instagram_link='https://www.instagram.com/'
         )
         cls.guest_client = Client()
+        cls.factory = APIRequestFactory()
+        cls.api_client = APIClient()
+
+    # def setUp(self):
+        # """Подготовка прогона теста. Вызывается перед каждым тестом."""
 
     def test_school_api(self):
         """Checks 200 response for api/v1/school get request."""
         response = SchoolTest.guest_client.get('/api/v1/school/')
         self.assertEqual(response.status_code, 200)
+        
+    def test_school_name(self):
+        response = SchoolTest.guest_client.get('/api/v1/school/')
+        # print(response.data.get('name')) # не работает, потому что без
+        # пагинации данные содержатся в объекте https://stackoverflow.com/questions/55286605/drf-jsonrenderer-returnlist-object-has-no-attribute-get
+        self.assertEqual(response.data[0].get('name'), 'Test_school')
 
+        
+    # def __init__(self, data=None, status=None,
+    #              template_name=None, headers=None,
+    #              exception=False, content_type=None):
 
+# class 
 
+# request = factory.post('/notes/', {'title': 'new idea'})
