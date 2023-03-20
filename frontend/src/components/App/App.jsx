021a24ece03d188798be89d66f5c30b0e0a44475
venera1306 <evis13-94@mail.ru>
2023-03-20 14:26:16 +0500

    course-page

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { MaingPage } from '../../pages/MainPage/MainPage';
 import { Header } from '../Header/Header';
 import './App.scss';
+import { CoursePage } from '../../pages/CoursePage/CoursePage';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <MaingPage />
+        <CoursePage />
       </main>
     </div>
   );
