bdd447efb0f4f250dcf016295b2d7028ca4c2072
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-03-28 18:24:31 +0300

    fix: slider bullet, autoplay

--- frontend/src/components/Slider/Slider.jsx
+++ frontend/src/components/Slider/Slider.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-unresolved */
 // eslint-disable-next-line import/no-unresolved
+import { Autoplay, Pagination } from 'swiper';
 import 'swiper/css';
+import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import slide from '../../images/slide1.png';
 import cls from './Slider.module.scss';
@@ -10,13 +13,17 @@ export const Slider = () => {
       <div className={cls.slider}>
         <Swiper
           className={cls.swiper}
-          // loop
           spaceBetween={40}
           slidesPerView="auto"
+          pagination={{
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          modules={[Pagination, Autoplay]}
         >
-          <SwiperSlide className={cls.slide}>
-            <img className={cls.image} src={slide} alt="Слайд" />
-          </SwiperSlide>
           <SwiperSlide className={cls.slide}>
             <img className={cls.image} src={slide} alt="Слайд" />
           </SwiperSlide>
