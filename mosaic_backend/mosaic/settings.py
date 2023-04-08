5c04bb9346ac7dce681c00be8bca2c365b648bbb
Yury Zharikov <zharikovyuri@gmail.com>
2023-04-08 19:34:59 +0300

    fix nginx conf

--- mosaic_backend/mosaic/settings.py
+++ mosaic_backend/mosaic/settings.py
@@ -131,7 +131,7 @@ if LOCAL_DEV is False:
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='mosaic_admin'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
         }
