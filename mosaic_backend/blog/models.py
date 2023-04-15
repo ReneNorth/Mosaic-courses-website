c83778cbbd72fa548aa49903469e68c1c545f881
Yury Zharikov <zharikovyuri@gmail.com>
2023-04-15 08:04:00 +0300

    add api/redoc

--- mosaic_backend/blog/models.py
+++ mosaic_backend/blog/models.py
@@ -13,19 +13,7 @@ class Post(models.Model):
     text = MDTextField(max_length=2000)
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     image = models.ImageField(
-        upload_to='blog/posts/', null=True, blank=True)
+        upload_to='blog/posts/', blank=True)
 
     def __str__(self):
         return f'{self.title}, {self.pub_date.strftime("%Y-%m-%d %H:%M:%S")}'
-    
-    
-    
-    # может потом добавить группы постам
-    # group = models.ForeignKey(
-    #     Group,
-    #     on_delete=models.SET_NULL,
-    #     null=True,
-    #     blank=True,
-    #     related_name='posts'
-    # )
-
