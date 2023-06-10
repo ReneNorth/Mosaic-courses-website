2521080137a6d5d7179bdc1abd8e15a48fd71350
Yury Zharikov <zharikovyuri@gmail.com>
2023-06-10 08:12:46 +0300

    fix settings

--- mosaic_backend/mosaic/settings.py
+++ mosaic_backend/mosaic/settings.py
@@ -61,7 +61,9 @@ MIDDLEWARE = [
 # CORS
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_URLS_REGEX = r'^/api/.*$'
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://localhost:3000']
+CSRF_TRUSTED_ORIGINS = ['http://localhost',
+                        'http://localhost:3000',
+                        'http://tessera.hopto.org/']
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost',
 #     'http://localhost:3000',
