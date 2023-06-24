a80df72602413d5e5864beccb0c5c7eb4bfe324d
Yury Zharikov <zharikovyuri@gmail.com>
2023-06-24 09:04:45 +0300

    add meditor

--- mosaic_backend/mosaic/urls.py
+++ mosaic_backend/mosaic/urls.py
@@ -13,16 +13,16 @@ from rest_framework import permissions
 
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title='Mosaic API',
-      default_version='v1',
-      description='Документация для api проекта Mosaic',
-      # terms_of_service="URL страницы с пользовательским соглашением",
-      contact=openapi.Contact(email='will be there'),
-      license=openapi.License(name='BSD License'),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title='Mosaic API',
+        default_version='v1',
+        description='Документация для api проекта Mosaic',
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email='will be there'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 
@@ -30,18 +30,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path(r'mdeditor/', include('mdeditor.urls')),
-    path(r'mdeditor/', include('mdeditor.urls'))
-    
+
 ]
 
 # TODO объединить
 urlpatterns += [
-   re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-           schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-           name='schema-swagger-ui'),
-   re_path(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-           name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),
 ]
 
 if settings.DEBUG:
