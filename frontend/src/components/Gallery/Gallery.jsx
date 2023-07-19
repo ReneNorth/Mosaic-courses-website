e76f41e3c07e700ec17495bc13b861d51754d172
SmthOfValue <bik00014@gmail.com>
2023-07-19 22:34:23 +0300

    fix: gallery slider loop bug

--- frontend/src/components/Gallery/Gallery.jsx
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-duplicates */
 /* eslint-disable import/no-unresolved */
 import React, { useEffect, useState } from 'react';
-import 'swiper/swiper-bundle.min.css';
+import { useSpringCarousel } from 'react-spring-carousel';
 import imageOne from '../../images/gallery_slider_1.png';
 import imageTwo from '../../images/gallery_slider_2.png';
 import imageFour from '../../images/gallery_slider_4.png';
@@ -10,6 +10,14 @@ import imageFive from '../../images/gallery_slider_5.png';
 import imageSix from '../../images/gallery_slider_6.png';
 import imagetru from '../../images/gallery_slider_6.png';
 import imagehru from '../../images/gallery_slider_6.png';
+// import imageOne from '../../images/digit1.jpg';
+// import imageTwo from '../../images/digit2.jpg';
+// import imageFour from '../../images/digit3.jpg';
+// import imageFive from '../../images/digit4.jpg';
+// import imageSix from '../../images/digit5.jpg';
+// import imageSeven from '../../images/digit6.jpg';
+// import imagetru from '../../images/digit7.jpg';
+// import imagehru from '../../images/gallery_slider_6.png';
 import cls from './Gallery.module.scss';
 
 export const data = [
@@ -33,27 +41,37 @@ export const data = [
     link: imageSix,
     id: 5,
   },
-  {
-    link: imageSix,
-    id: 6,
-  },
-  {
-    link: imagetru,
-    id: 7,
-  },
-  {
-    link: imagehru,
-    id: 8,
-  },
 ];
 
 export const Gallery = () => {
-  const [sliderIndex, setSliderIndex] = useState(0);
+  const [sliderIndex, setSliderIndex] = useState(1);
+
+  const {
+    carouselFragment,
+    slideToPrevItem,
+    slideToNextItem,
+  } = useSpringCarousel({
+    itemsPerSlide: 5,
+    withLoop: true,
+    items: data.map((image, index) => ({
+      id: image.id,
+      renderItem: (
+        <div className={cls.item} key={image.id}>
+          <img
+            className={cls.image}
+            src={image.link}
+            alt="картинка слайдер"
+          />
+        </div>
+      ),
+    })),
+  });
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setSliderIndex(sliderIndex + 1);
-    }, 5000);
+      slideToNextItem();
+    }, 10000);
 
     const lastSliderIndex = data.length - 1;
     if (sliderIndex < 0) {
@@ -64,7 +82,7 @@ export const Gallery = () => {
     }
 
     return () => clearTimeout(timer);
-  }, [sliderIndex]);
+  }, [slideToNextItem, sliderIndex]);
 
   return (
     <section className={cls.sectiongallery}>
@@ -75,52 +93,20 @@ export const Gallery = () => {
           <span> вдохновения</span>
         </h2>
       </div>
-
       <div className={cls.slider}>
-        <ul className={cls.slider__top}>
-          {data.map((slide, index) => {
-            let position;
-            const activeIndex = 0;
-            if (index === sliderIndex) {
-              position = 'active';
-            }
-            if (index > sliderIndex) {
-              position = 'next';
-            }
-            if (index > sliderIndex + 1) {
-              position = 'next2';
-            }
-            if (index < sliderIndex) {
-              position = 'last';
-            }
-            if (index < sliderIndex - 1) {
-              position = 'last2';
-            }
-
-            return (
-              <li
-                key={slide.id}
-                className={`${cls.slider__item} ${sliderIndex === index
-                  ? `${cls.active}`
-                  : `${cls[position]}`} ${sliderIndex === 0 || sliderIndex === 1 ? `${cls.slider__item_start}` : ''}
-               ${sliderIndex <= data.length - 1 && sliderIndex >= data.length - 2 ? `${cls.slider__item_end}` : ''}`}
-              >
-                <img src={slide.link} alt="картинка слайдер" />
-              </li>
-            );
-          })}
-        </ul>
-
+        {carouselFragment}
         <ul className={cls.slider__dots}>
           <li>
             <button
-              onClick={() => setSliderIndex((prev) => prev - 1)}
-              type="button"
-              aria-label="prev slide"
               className={cls.slider__dots_prev}
+              type="button"
+              onClick={() => {
+                slideToPrevItem();
+                setSliderIndex(sliderIndex - 1);
+              }}
+              aria-label="prev slide"
             />
           </li>
-
           {data.map((slide, index) => (
             <li key={slide.id}>
               <button
@@ -131,13 +117,15 @@ export const Gallery = () => {
               />
             </li>
           ))}
-
           <li>
             <button
-              onClick={() => setSliderIndex((prev) => prev + 1)}
-              type="button"
-              aria-label="next slide"
               className={cls.slider__dots_next}
+              type="button"
+              onClick={() => {
+                slideToNextItem();
+                setSliderIndex(sliderIndex + 1);
+              }}
+              aria-label="next slide"
             />
           </li>
         </ul>
