9f65d4619db3da3b329aff53e1cb54c820b70764
Yury Zharikov <zharikovyuri@gmail.com>
2023-07-25 07:04:25 +0300

    fix flake code issues

--- mosaic_backend/mosaic/urls.py
+++ mosaic_backend/mosaic/urls.py
@@ -1,15 +1,8 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-
-# from django.conf.urls import url
-
-
-# from django.conf.urls import url
 
 
 schema_view = get_schema_view(
@@ -40,7 +33,3 @@ urlpatterns += [
     re_path(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
             name='schema-redoc'),
 ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
