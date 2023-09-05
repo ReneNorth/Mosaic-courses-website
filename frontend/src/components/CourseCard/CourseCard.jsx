0f0c4182d64a66c0ca0ce14e95b8803f3942d49d
SmthOfValue <bik00014@gmail.com>
2023-09-05 13:39:42 +0300

    added lowest price display

--- frontend/src/components/CourseCard/CourseCard.jsx
+++ frontend/src/components/CourseCard/CourseCard.jsx
@@ -1,24 +1,30 @@
 import { Button } from '../Button/Button';
 import { formatCourseDate } from '../../helpers/formatDate';
 import cls from './CourseCard.module.scss';
-import { findEarliestDate } from '../../helpers/earliestDate';
+import { findEarliestDate } from '../../helpers/findEarliestDate';
+import { findLowestCoursePrice } from '../../helpers/findLowestCoursePrice';
 
 export const CourseCard = ({
   item, type = 'default', handleEnroll, handleGetMore,
 // eslint-disable-next-line consistent-return
 }) => {
   let date;
+  let price;
+  let currency;
   if (item.masterclasses.length > 0) {
     const startDatesArray = item.masterclasses.map((masterclass) => masterclass.time_start);
     const earliestStartDate = findEarliestDate(startDatesArray);
     date = formatCourseDate(new Date(earliestStartDate));
+
+    const pricesWithCurrency = item.masterclasses.map((mc) => [Number(mc.price), mc.currency]);
+    [price, currency] = findLowestCoursePrice(pricesWithCurrency);
   }
 
   if (item.masterclasses.length > 0) {
     return (
       <>
         <div className={cls.imgWrapper}>
-          <img className={cls.img} src={item.img} alt={item.title} />
+          <img className={cls.img} src={item.image} alt={item.title} />
           {type === 'all' && (
             <div className={cls.start}>
               <span>Ближайший старт</span>
@@ -42,7 +48,7 @@ export const CourseCard = ({
                 : (
                   <div className={cls.priceContainer}>
                     <span className={cls.priceIcon} />
-                    <p className={cls.info}>{`${item.price} ${item.currency}`}</p>
+                    <p className={cls.info}>{`${price} ${currency}`}</p>
                   </div>
                 )}
             </div>
