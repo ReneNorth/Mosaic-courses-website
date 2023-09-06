4bab9d9bdce12d84bcaf161727bf220496b32bbe
SmthOfValue <bik00014@gmail.com>
2023-09-06 22:45:17 +0300

    added reverted changes

--- frontend/src/services/store.js
+++ frontend/src/services/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { postSliceReducer } from './slices/postsSlice';
+import { courseSliceReducer } from './slices/coursesSlide';
 
 export const store = configureStore({
   reducer: {
     posts: postSliceReducer,
+    courses: courseSliceReducer,
   },
 });
