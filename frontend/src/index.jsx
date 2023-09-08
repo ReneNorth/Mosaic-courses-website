b5d23a4d8e40264832561ac10fa39288fffa550e
SmthOfValue <bik00014@gmail.com>
2023-09-08 14:04:37 +0300

    added scroll to top on navigation, related blog posts are clickable now, fixed courses styles

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,12 +5,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import './index.scss';
 import { store } from './services/store';
+import { ScrollToTop } from './components/ScrollToTop/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>
