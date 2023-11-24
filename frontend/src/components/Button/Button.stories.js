1a7c0bcc952bd0fd91e4e7286f2b1220a714a5db
zigfrei <zigfrei.zf@gmail.com>
2023-11-24 23:00:06 +0300

    feat: add scripts for gh-pages

--- frontend/src/components/Button/Button.stories.js
+++ frontend/src/components/Button/Button.stories.js
@@ -10,16 +10,81 @@ export default {
   },
   tags: ['autodocs'],
   argTypes: {
-    decoration: { control: 'radio', options: ['black', 'white', null] },
-    className: { control: 'radio', options: ['fill', 'outline'] },
+    decoration: {
+      type: 'string',
+      description: 'Цвет заднего контура',
+      control: 'radio',
+      options: ['black', 'white', null],
+      table: {
+        type: { summary: ['black', 'white', 'null'] },
+        defaultValue: { summary: 'null' },
+      },
+    },
+    className: {
+      type: 'string',
+      description: 'Состояние заливки кнопки',
+      control: 'radio',
+      options: ['fill', 'outline'],
+      table: {
+        type: { summary: ['fill', 'outline'] },
+        defaultValue: { summary: 'fill' },
+      },
+    },
+    disabled: {
+      type: { name: 'boolean' },
+      description: 'Отключение действия кнопки',
+      table: {
+        type: { summary: ['true', 'false'] },
+        defaultValue: { summary: 'false' },
+      },
+      options: ['false', 'true'],
+    },
+    children: {
+      type: 'string',
+      name: 'label',
+      description: 'Текст внутри кнопки',
+    },
+    type: {
+      type: 'string',
+      description: 'Тип кнопки',
+      options: ['button', 'submit'],
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'button' },
+      },
+      control: {
+        type: 'radio',
+      },
+    },
   },
 };
 
-export const MainButton = {
+export const fieldButtonWithoutDecoration = {
   args: {
-    decoration: 'black',
+    children: 'Нажми на меня',
     className: 'fill',
-    disabled: false,
-    children: 'Нажми на меня!',
+  },
+};
+
+export const fieldButtonWithDecoration = {
+  args: {
+    children: 'Нажми на меня',
+    className: 'fill',
+    decoration: 'black',
+  },
+};
+
+export const outlineButton = {
+  args: {
+    children: 'Нажми на меня',
+    className: 'outline',
+  },
+};
+
+export const disabledButton = {
+  args: {
+    children: 'Нажми на меня',
+    className: 'fill',
+    disabled: true,
   },
 };
