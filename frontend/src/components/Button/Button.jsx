e375d120aeb118621b4465800a1fb261480996a3
RitaKruglova <kruglova404@mail.ru>
2024-04-12 17:58:38 +0300

    feat: add footer styles for tablet resolution

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,17 +1,23 @@
 import { classNames } from '../../helpers/classNames';
-import cls from './Button.module.scss';
+import styles from './Button.module.scss';
 
 export const Button = ({
   children, className = '', decoration, ...props
 }) => {
+  const classes = className.split(' ');
   return (
     <button
       type="button"
       className={
         classNames(
-          cls.button,
+          styles.button,
           {},
-          [cls[className], decoration === 'black' && cls.decoration, decoration === 'white' && cls.decorationWhite],
+          [
+            styles[classes[0]],
+            decoration === 'black' && styles.decoration,
+            decoration === 'white' && styles.decorationWhite,
+            ...classes.slice(1).map((cls) => styles[cls]),
+          ],
         )
       }
       {...props}
