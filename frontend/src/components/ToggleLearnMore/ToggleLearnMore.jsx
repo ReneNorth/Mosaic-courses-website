fd285ababffe221828b12b77dd316f9d91ef4fdc
RitaKruglova <kruglova404@mail.ru>
2024-05-06 17:44:08 +0300

    fix: change SectionInfo component to fit the design

--- frontend/src/components/ToggleLearnMore/ToggleLearnMore.jsx
+++ frontend/src/components/ToggleLearnMore/ToggleLearnMore.jsx
@@ -9,18 +9,6 @@ export const ToggleLearnMore = () => {
   };
   return (
     <div>
-      <h3 className={cls.containerHeading}>Требования к ученику</h3>
-      <div className={cls.sectionContainer}>
-        <div className={cls.text}>
-          Мы проводим мастер-классы по живописи и гончарному делу для деток и
-          взрослых.
-          <p>
-            Мы делаем рисование доступным с помощью пошаговой программы и
-            вовлечённых преподавателей. Вы как ученик обязательно прочувствуете
-            нашу дружескую и лёгкую атмосферу.
-          </p>
-        </div>
-      </div>
       <h3 className={cls.containerHeading}>О курсе</h3>
       <div className={cls.sectionContainer}>
         <div className={cls.text}>
