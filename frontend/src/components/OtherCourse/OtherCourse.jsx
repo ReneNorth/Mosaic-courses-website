d96ee818d6a86d5abeec46644b6f77648689e6ef
RitaKruglova <kruglova404@mail.ru>
2024-05-07 15:11:33 +0300

    refactor: add OtherCourse component and make OtherCourses to fit the design

---
+++ frontend/src/components/OtherCourse/OtherCourse.jsx
@@ -0,0 +1,41 @@
+import otherCourseStyles from './OtherCourse.module.scss';
+
+const OtherCourse = ({
+  imagePath, title, price, duration, description,
+}) => {
+  return (
+    <div className={otherCourseStyles.container}>
+      <div className={otherCourseStyles.wrapper}>
+        <img
+          className={otherCourseStyles.image}
+          src={imagePath}
+          alt="Промо картинка"
+        />
+      </div>
+      <div className={otherCourseStyles.courseBox}>
+        <h4 className={otherCourseStyles.title}>
+          {title}
+        </h4>
+        <div className={otherCourseStyles.wrapperAtribut}>
+          <div className={otherCourseStyles.priceContainer}>
+            <div className={otherCourseStyles.timeIcon} />
+            <p>{duration}</p>
+          </div>
+          <div className={otherCourseStyles.priceContainer}>
+            <div className={otherCourseStyles.rubleIcon} />
+            <p>{price}</p>
+          </div>
+        </div>
+
+        <p className={otherCourseStyles.description}>
+          {description}
+        </p>
+        <button className={otherCourseStyles.btn} type="button">
+          Узнать подробнее
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OtherCourse;
--- frontend/src/components/OtherCourses/OtherCourses.jsx
+++ frontend/src/components/OtherCourses/OtherCourses.jsx
@@ -1,82 +1,34 @@
 import courseImgOne from '../../images/course-img-1.png';
 import courseImgTwo from '../../images/course-img-2.png';
+import courseImgThree from '../../images/course-img-3.png';
+import OtherCourse from '../OtherCourse/OtherCourse';
 import cls from './OtherCourses.module.scss';
 
 export const OtherCourses = () => {
   return (
     <div className={cls.section}>
       <div className={cls.containerInfo}>
-        <div className={cls.contanerCourseOne}>
-          <div className={cls.wrapper}>
-            <img
-              className={cls.courseImage}
-              src={courseImgOne}
-              alt="Промо картинка"
-            />
-          </div>
-          <div className={cls.courseBox}>
-            <h4 className={cls.courseName}>
-              Недельный Курс по мозаике из стекла
-            </h4>
-            <div className={cls.wrapperAtribut}>
-              <div className={cls.priceContainer}>
-                {/* <div className={cls.iconContainer}> */}
-                <div className={cls.timeIcon} />
-                {/* </div> */}
-                <p>3 часа</p>
-              </div>
-              <div className={cls.priceContainer}>
-                <div className={cls.rubleIcon} />
-
-                <p>4000 рублей</p>
-              </div>
-            </div>
-
-            <p className={cls.description}>
-              Мы делаем рисование доступным с помощью пошаговой программы и
-              вовлечённых преподавателей.
-            </p>
-            <button className={cls.btnWhite} type="button">
-              Узнать подробнее
-            </button>
-          </div>
-        </div>
-        <div className={cls.contanerCourseTwo}>
-          <div className={cls.wrapper}>
-            <img
-              className={cls.courseImage}
-              src={courseImgTwo}
-              alt="Промо картинка"
-            />
-          </div>
-          <div className={cls.courseBox}>
-            <h4 className={cls.courseName}>
-              Недельный Курс по мозаике из стекла
-            </h4>
-            <div className={cls.wrapperAtribut}>
-              <div className={cls.priceContainer}>
-                {/* <div className={cls.iconContainer}> */}
-                <div className={cls.timeIcon} />
-                {/* </div> */}
-                <p>3 часа</p>
-              </div>
-              <div className={cls.priceContainer}>
-                {/* <div className={cls.iconContainer}> */}
-                <div className={cls.rubleIcon} />
-                {/* </div> */}
-                <p>4000 рублей</p>
-              </div>
-            </div>
-            <p className={cls.description}>
-              Мы делаем рисование доступным с помощью пошаговой программы и
-              вовлечённых преподавателей.
-            </p>
-
-            <button className={cls.btnWhite} type="button">
-              Узнать подробнее
-            </button>
-          </div>
-        </div>
+        <OtherCourse
+          imagePath={courseImgOne}
+          title="недельный Курс по мозаике из стекла"
+          price="4000 рублей"
+          duration="3 часа"
+          description="Мы делаем рисование доступным с помощью пошаговой программы и вовлечённых преподавателей."
+        />
+        <OtherCourse
+          imagePath={courseImgTwo}
+          title="недельный Курс по мозаике из стекла"
+          price="4000 рублей"
+          duration="3 часа"
+          description="Мы делаем рисование доступным с помощью пошаговой программы и вовлечённых преподавателей."
+        />
+        <OtherCourse
+          imagePath={courseImgThree}
+          title="недельный Курс по мозаике из стекла"
+          price="4000 рублей"
+          duration="3 часа"
+          description="Мы делаем рисование доступным с помощью пошаговой программы и вовлечённых преподавателей."
+        />
       </div>
     </div>
   );
