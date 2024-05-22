8b314b689f2eef18830014a0bac799d71e399f30
RitaKruglova <kruglova404@mail.ru>
2024-05-22 15:42:14 +0300

    fix: correct overlay for CalendarPopup

--- frontend/src/components/CoursePageFinal/CoursePageFinal.jsx
+++ frontend/src/components/CoursePageFinal/CoursePageFinal.jsx
@@ -1,36 +1,22 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { SectionInfo } from '../SectionInfo/SectionInfo';
 import { CoursePageInfo } from '../CoursePageInfo/CoursePageInfo';
 import { StickySidebar } from '../StickySidebar/StickySidebar';
 import coursePageFinalStyles from './CoursePageFinal.module.scss';
 import CalendarPopup from '../CalendarPopup/CalendarPopup';
-import { useResize } from '../../hooks/useResize';
 
 export const CoursePageFinal = () => {
-  const { width } = useResize();
   const isCalendarPopupOpen = useSelector((store) => store.popup.isCalendarPopupOpen);
 
-  if (width > 1320) {
-    return (
-      <div className={coursePageFinalStyles.page}>
-        <div className={coursePageFinalStyles.container}>
-          <CoursePageInfo />
-          <SectionInfo />
-        </div>
-        <StickySidebar />
-        {isCalendarPopupOpen && <CalendarPopup />}
-      </div>
-    );
-  }
-  if (width <= 1320 && isCalendarPopupOpen) {
-    return (
-      <div className={coursePageFinalStyles.page}>
-        <div className={coursePageFinalStyles.popupContainer}>
-          <CalendarPopup />
-        </div>
-      </div>
-    );
-  }
+  useEffect(() => {
+    if (isCalendarPopupOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [isCalendarPopupOpen]);
+
   return (
     <div className={coursePageFinalStyles.page}>
       <div className={coursePageFinalStyles.container}>
@@ -38,6 +24,7 @@ export const CoursePageFinal = () => {
         <SectionInfo />
       </div>
       <StickySidebar />
+      {isCalendarPopupOpen && <CalendarPopup />}
     </div>
   );
 };
