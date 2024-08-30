9d1dec8c0c3582ef9ea301927984e4619cdf546a
Yury Zharikov <zharikovyuri@gmail.com>
2024-08-30 23:57:40 +0300

    add button to ProfileModal component

--- frontend/src/components/ProfileModal/ProfileModal.jsx
+++ frontend/src/components/ProfileModal/ProfileModal.jsx
@@ -5,6 +5,7 @@ import { classNames } from '../../helpers/classNames';
 import IconSettings from '../../images/profile_modal_settings.svg';
 import IconSignIn from '../../images/profile_modal_sign_in_course.svg';
 import IconLogOut from '../../images/profile_modal_log_out_profile.svg';
+import Calendar from '../../images/calendar.svg';
 
 export const ProfileModal = ({ isOpen, setIsOpen }) => {
   return (
@@ -18,17 +19,24 @@ export const ProfileModal = ({ isOpen, setIsOpen }) => {
         <div className={cls.contentWrapper}>
           <NavLink
             className={cls.link}
+            to="/register"
+          >
+            <img src={IconSignIn} alt="Регистрация/логин" />
+            Регистрация/логин
+          </NavLink>
+          <NavLink
+            className={cls.link}
+            to="/my-masterclasses"
+          >
+            <img src={Calendar} alt="Записаться на курс" />
+            Мои мастерклассы
+          </NavLink>
+          <NavLink
+            className={cls.link}
             to="/profile"
           >
             <img src={IconSettings} alt="В настройки профиля" />
             Настройки аккаунта
-          </NavLink>
-          <NavLink
-            className={cls.link}
-            to="/course-registration"
-          >
-            <img src={IconSignIn} alt="Записаться на курс" />
-            Записи на курсы
           </NavLink>
           <button className={cls.button} type="button">
             <img src={IconLogOut} alt="Выйти из профиля" />
