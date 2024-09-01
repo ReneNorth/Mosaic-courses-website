f4cc38792839256e99a165e41b7e6267c51d7279
Romanovskiy Ivan <ivanromanovskiy1103@gmail.com>
2024-09-01 13:05:26 +0300

    feat: TD-235 added the ability to close the popup when clicking on a overlay

--- frontend/src/components/ProfileModal/ProfileModal.jsx
+++ frontend/src/components/ProfileModal/ProfileModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import cls from './ProfileModal.module.scss';
 import { classNames } from '../../helpers/classNames';
@@ -7,43 +7,50 @@ import IconSignIn from '../../images/profile_modal_sign_in_course.svg';
 import IconLogOut from '../../images/profile_modal_log_out_profile.svg';
 import Calendar from '../../images/calendar.svg';
 
-export const ProfileModal = ({ isOpen, setIsOpen }) => {
+export const ProfileModal = ({ isOpen, onClose }) => {
+  const popup = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (popup.current && !popup.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
   return (
-    <>
-      <div
-        className={classNames(cls.overlay, { [cls.overlayOpen]: isOpen }, [])}
-        onClick={() => setIsOpen(!isOpen)}
-        data-testid="overlay"
-      />
-      <div className={classNames(cls.popup, { [cls.popupOpen]: isOpen }, [])} data-testid="popup">
-        <div className={cls.contentWrapper}>
-          <NavLink
-            className={cls.link}
-            to="/register"
-          >
-            <img src={IconSignIn} alt="Регистрация/логин" />
-            Регистрация/логин
-          </NavLink>
-          <NavLink
-            className={cls.link}
-            to="/my-masterclasses"
-          >
-            <img src={Calendar} alt="Записаться на курс" />
-            Мои мастерклассы
-          </NavLink>
-          <NavLink
-            className={cls.link}
-            to="/profile"
-          >
-            <img src={IconSettings} alt="В настройки профиля" />
-            Настройки аккаунта
-          </NavLink>
-          <button className={cls.button} type="button">
-            <img src={IconLogOut} alt="Выйти из профиля" />
-            Выйти
-          </button>
-        </div>
+    <div
+      className={classNames(cls.popup, { [cls.popupOpen]: isOpen }, [])}
+      ref={popup}
+    >
+      <div className={cls.contentWrapper}>
+        <NavLink className={cls.link} to="/register">
+          <img src={IconSignIn} alt="Регистрация/логин" />
+          Регистрация/логин
+        </NavLink>
+        <NavLink className={cls.link} to="/my-masterclasses">
+          <img src={Calendar} alt="Записаться на курс" />
+          Мои мастер-классы
+        </NavLink>
+        <NavLink className={cls.link} to="/profile">
+          <img src={IconSettings} alt="В настройки профиля" />
+          Настройки аккаунта
+        </NavLink>
+        <button className={cls.button} type="button">
+          <img src={IconLogOut} alt="Выйти из профиля" />
+          Выйти
+        </button>
       </div>
-    </>
+    </div>
   );
 };
