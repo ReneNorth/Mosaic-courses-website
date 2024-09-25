26630f0c856fbb8b56443d66c282798b4aeb5912
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-09-25 14:05:57 +0500

    dispaly for window without past masterclasses

--- frontend/src/pages/MyMasterclassesPastPage/MyMasterclassesPastPage.jsx
+++ frontend/src/pages/MyMasterclassesPastPage/MyMasterclassesPastPage.jsx
@@ -4,21 +4,34 @@ import Calendar from '../../components/Calendar/Calendar';
 import { MyMasterclassesHeaderLinks } from '../../components/MyMasterclassesHeaderLinks/MyMasterclassesHeaderLinks';
 import { masterclasses } from '../../utils/consts/mockMasterclasses';
 import MasterclassList from '../../components/MasterclassList/MasterclassList';
+import { CardMoreContent } from '../../components/CardMoreContent/CardMoreContent';
 
 export const MyMasterclassesPastPage = () => {
   const message = 'Нет прошедших занятий';
   return (
-    <div className={cls.flexContainer}>
-      <div className={cls.calendarContainer}>
-        <Calendar />
+    <>
+      <div className={cls.flexContainer}>
+        <div className={cls.calendarContainer}>
+          <Calendar />
+        </div>
+        <div className={cls.masterclassContainer}>
+          <h2 className={cls.title}>Записи на мастер-классы</h2>
+          <ul className={classNames(cls.list, {}, [])}>
+            <MyMasterclassesHeaderLinks />
+          </ul>
+          <MasterclassList masterclasses={masterclasses} message={message} />
+        </div>
       </div>
-      <div className={cls.masterclassContainer}>
-        <h2 className={cls.title}>Записи на мастер-классы</h2>
-        <ul className={classNames(cls.list, {}, [])}>
-          <MyMasterclassesHeaderLinks />
-        </ul>
-        <MasterclassList masterclasses={masterclasses} message={message} />
-      </div>
-    </div>
+      {masterclasses.length === 0 && (
+        <div className={cls.wrapper}>
+          <h3 className={cls.subtitle}>Также покупают</h3>
+          <div className={cls.cardMoreContentContainer}>
+            <CardMoreContent />
+            <CardMoreContent />
+            <CardMoreContent />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
