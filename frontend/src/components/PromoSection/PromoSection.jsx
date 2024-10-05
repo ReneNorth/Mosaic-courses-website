7e5a780d2ce1b1d9c2fdc0a086f244be54cec34c
Denis <123562174+borove4ik@users.noreply.github.com>
2024-10-05 09:03:03 +0300

    убраны лигшние пропсы

--- frontend/src/components/PromoSection/PromoSection.jsx
+++ frontend/src/components/PromoSection/PromoSection.jsx
@@ -2,8 +2,6 @@ import cls from './PromoSection.module.scss';
 /**
  * Renders a promo section with the provided images, title, text, and otherElements.
  *
- * @param {object} desktopImage - The desktop image for the promo section.
- * @param {object} mobileImage - The mobile image for the promo section.
  * @param {string} title - The title for the promo section, you can wrap the word in a span and it will be highlighted.
  * @param {string} text - The text content for the promo section.
  * @param {object} otherElements - This element for buttons, information curse, information post and other. This element must be styled before being sent to the component.
@@ -17,15 +15,11 @@ export const PromoSection = ({
     <section className={cls.section}>
       <div className={cls.left}>
         <h1 className={cls.title}>{title}</h1>
-        {/* <img src={mobileImage} alt="мобильная декоративная картинка" className={cls.mobileImage} /> */}
         <p className={cls.paragraph}>{text}</p>
         <div className={cls.otherElements}>
           {otherElements}
         </div>
       </div>
-      {/* <div className={cls.right}>
-        <img src={`${desktopImage}`} alt="десктопная декоративная картинка" className={cls.desktopImage} />
-      </div> */}
     </section>
   );
 };
