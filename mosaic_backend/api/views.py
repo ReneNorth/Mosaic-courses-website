217cd78ece97fb99d1132061e55b1eabf8e4ed2d
Yury Zharikov <zharikovyuri@gmail.com>
2024-10-11 22:03:04 +0300

    fix flake8 errors

--- mosaic_backend/api/views.py
+++ mosaic_backend/api/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, mixins, status, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny
--- mosaic_backend/marketplace/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-
-from marketplace.models import Artwork, ArtworkMainPage
