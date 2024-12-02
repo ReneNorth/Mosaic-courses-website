f235a1d4f2660db971b6260deb10b88bf3177ce1
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-12-02 14:33:50 +0300

    added css style cardInfo for deckstop

--- frontend/src/components/GalleryCardInfo/GalleryCardInfo.jsx
+++ frontend/src/components/GalleryCardInfo/GalleryCardInfo.jsx
@@ -8,7 +8,7 @@ export const GalleryCardInfo = () => {
   }
 
   return (
-    <>
+    <div className={cls.wrapper}>
       <div className={cls.container}>
         <div className={cls.flexContainer}>
           <h2 className={cls.title}>Название</h2>
@@ -23,30 +23,34 @@ export const GalleryCardInfo = () => {
           </div>
         </div>
       </div>
-      <div>
-        <p>Мозаика</p>
-        <p>Кто выполнил</p>
+      <div className={cls.author}>
+        <span>Мозаика</span>
+        <span>Работа преподавателя</span>
       </div>
-      <p>Цена</p>
-      <button type="button" aria-label="Купить">Купить в один клик</button>
-      <button type="button" aria-label="Добавить в корзину">Добавить в корзину</button>
-      {/* добавить нижнюю гарницу с пэддингом */}
-      {/* <table>
-        <tr>
-          <td>Автор</td>
-          <td>{}</td>
+      <p className={cls.price}>2000 P</p>
+      <button className={cls.buttonBuyByClick} type="button" aria-label="Купить">Купить в один клик</button>
+      <button className={cls.button} type="button" aria-label="Добавить в корзину">Добавить в корзину</button>
+      {/* <button className={cls.button} type="button" aria-label="Заказать изготовление">Заказать изготовление</button>
+      <button className={cls.button} type="button" aria-label="Выбрать занятие">Выбрать занятие</button> */}
+      <table className={cls.table}>
+        <tr className={cls.tableCell}>
+          <td className={cls.tableTitle}>Автор</td>
+          <td className={cls.tableText}>Анна Илларионова</td>
         </tr>
-        <tr>
-          <td>Размер</td>
-          <td>{}</td>
+        <tr className={cls.tableCell}>
+          <td className={cls.tableTitle}>Размер</td>
+          <td className={cls.tableText}>80Х80см</td>
         </tr>
-        <tr>
-          <td>Материал мозаики</td>
-          <td>{}</td>
+        <tr className={cls.tableCell}>
+          <td className={cls.tableTitle}>Материал мозаики</td>
+          <td className={cls.tableText}>Натуральный камень</td>
         </tr>
-      </table> */}
-      <p>Описание</p>
-      <p>{}</p>
-    </>
+      </table>
+      <p className={cls.description}>Описание</p>
+      <p className={cls.descriptionText}>
+        Мозаичное панно, природный узор по мотивам национальных росписей. Работа смонтирована на
+        деревянную основу. Цена указана без багета.
+      </p>
+    </div>
   );
 };
