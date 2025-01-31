fa2848aab5d8a49e4b13db690c1c1a4269c4ce66
Yury Zharikov <zharikovyuri@gmail.com>
2025-01-31 21:26:11 +0200

    add max value validation for certificates

--- mosaic_backend/mosaic/business_logic.py
+++ mosaic_backend/mosaic/business_logic.py
@@ -1,7 +1,9 @@
 class BusinessLogic():
     MIN_CERT_TG = 2000  # minimal amount of to issue a certificate
+    MAX_CERT_TG = 500000
     CERT_LIFETIME_DAYS = 180  # days before the certificate will expire
     ADMIN_NAME = 'Tessera Admin'  # a nickname for a case when admin's
+
     #  username is missing
 
 
