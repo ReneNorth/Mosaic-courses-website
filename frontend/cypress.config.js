7f88e8a6f63f7b22eea0cad8fae5293b9d68b6bf
Denis <123562174+borove4ik@users.noreply.github.com>
2025-03-29 01:17:00 +0500

    test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    devServer: {
+      framework: 'react', // или другой фреймворк
+      bundler: 'webpack',
+      allowedHosts: ['localhost'], // Убедитесь, что здесь указаны корректные хосты
+    },
   },
 });
